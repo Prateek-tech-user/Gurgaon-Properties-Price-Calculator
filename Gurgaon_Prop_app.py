2ceec2fe95cd92f975ee2e39134f5fa9502de53f
Prateek-tech-user <Prateek556kumar@gmail.com>
2025-03-30 18:08:56 +0530

    Update Gurgaon_Prop_app.py

--- Gurgaon_Prop_app.py
+++ Gurgaon_Prop_app.py
@@ -38,6 +38,6 @@ if st.button("Predict"):
     one_df = pd.DataFrame(data,columns=columns)
     #st.dataframe(one_df)
     price = np.expm1(pipline.predict(one_df)) 
-    st.text("Price")
+    st.text(price)
 
 

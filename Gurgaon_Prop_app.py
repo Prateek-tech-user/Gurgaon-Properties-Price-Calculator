c3efe0241d784845aea00ed117187b55c0a34c7f
Prateek-tech-user <Prateek556kumar@gmail.com>
2025-06-21 14:10:25 +0530

    Update Gurgaon_Prop_app.py

--- Gurgaon_Prop_app.py
+++ Gurgaon_Prop_app.py
@@ -37,7 +37,7 @@ if st.button("Predict"):
        'furnishing_type', 'luxury_category', 'floor_category']
     one_df = pd.DataFrame(data,columns=columns)
     #st.dataframe(one_df)
-    price = np.expm1(pipeline.predict(one_df))[0]  # extract scalar
+    price = np.expm1(pipline.predict(one_df))[0]  # extract scalar
     st.markdown(f"**Estimated price is ₹{np.round(price * 10000000):,.0f}**")
  
 
